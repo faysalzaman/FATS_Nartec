129d729240c0008c1fb30edd737290d88b4cffa9
faysalzaman <faysalzaman00@gmail.com>
2024-07-11 00:06:45 +0500

    Updated

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,7 @@ class Constant {
   // static String baseUrl = 'http://161.97.172.46:7001/api';
   // static String host = '161.97.172.46';
   static String baseUrl = "http://gs1ksa.org:7001/api";
-  static String host = 'gs1ksa.org:7001';
+  static String host = 'gs1ksa.org';
 
   static Color primaryColor = const Color(0xFF00006A);
 
